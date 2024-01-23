acc4bcbe28534d05acdfb9e58663cfcc0906edfe
ryo-hirata <149023236+ryo-hirata@users.noreply.github.com>
2024-01-23 19:33:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,3 @@ async def predict(file: UploadFile = File(...)):
 
     # Return JSON response
     return JSONResponse(content=content)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
